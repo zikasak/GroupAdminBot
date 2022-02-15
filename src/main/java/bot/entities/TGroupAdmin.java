e65675933dee4227af822a9f2bafe59c8d20955d
grigory <zikasaks@gmail.com>
2022-02-15 21:11:17 +0300

    Remove unnecessary field

--- src/main/java/bot/entities/TGroupAdmin.java
+++ src/main/java/bot/entities/TGroupAdmin.java
@@ -3,16 +3,11 @@ package bot.entities;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Set;
 
 @Data
 public class TGroupAdmin implements Serializable {
 
     private Long user_id;
     private Long chat_id;
-
-//    @JoinColumn(name = "chat_id", updatable = false, insertable = false)
-    private Set<TGroup> groups;
-
 
 }
